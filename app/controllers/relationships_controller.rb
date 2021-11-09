ffc6ba7f0c62d01d69f8d8cb8628e343c8ba4660
Ebireni David <ebirenidavid@gmail.com>
2021-11-09 22:16:52 +0100

    made follow button realtime

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -6,10 +6,12 @@ class RelationshipsController < ApplicationController
     
     if @rel
       @rel.destroy
+      ActionCable.server.broadcast("LikeChannel", {text: "follow", followers: User.find(params[:id]).followers.count, following: User.find(params[:id]).following.count})
     else
       @new_rel = Relationship.new(follower_id: current_user.id, followed_id: params[:id])
-
       @new_rel.save
+
+      ActionCable.server.broadcast("LikeChannel", {text: "unfollow", followers: User.find(params[:id]).followers.count, following: User.find(params[:id]).following.count})
     end
   end
 
--- app/javascript/channels/like_channel.js
+++ app/javascript/channels/like_channel.js
@@ -12,6 +12,12 @@ consumer.subscriptions.create("LikeChannel", {
 
   received(data) {
     // Called when there's incoming data on the websocket for this channel
-    console.log(data)
+    const p_tag = document.getElementById("follow-btn")
+    const following_show = document.getElementById("following_show")
+    const followers_show = document.getElementById("followers_show")
+  
+    p_tag.innerHTML = data.text
+    followers_show.innerHTML = "Followers: "+data.followers
+    following_show.innerHTML = "Following: "+data.following
   }
 });
