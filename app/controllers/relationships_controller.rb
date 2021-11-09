3c8324434ab710a6dc5a722c3903ce7fe65da773
Ebireni David <ebirenidavid@gmail.com>
2021-11-09 19:36:26 +0100

    changed controller method and added new route for the follow users feature

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,16 +1,12 @@
 class RelationshipsController < ApplicationController
-  def create
-    @rel = Relationship.new(follower_id: current_user.id, followed_id: params[:user_id])
-
-    if @rel.save
-      flash[:notice] = "you just followed #{User.find(params[:user_id]).username}"
-    end
-  end
-
-  def destroy
+  def follow
     @rel = Relationship.find_by(followed_id: params[:user_id], follower_id: current_user.id)
-  
-    if @rel.destroy
+    
+    if @rel
+      @rel.destroy
+    else
+      @new_rel = Relationship.new(follower_id: current_user.id, followed_id: params[:user_id])
+      @new_rel.save
     end
   end
 end
