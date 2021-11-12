88eea882fe857a324413a1b8bc3517dd8613ee86
Ebireni David <ebirenidavid@gmail.com>
2021-11-12 18:21:45 +0100

    more clean up

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_08_222427) do
+ActiveRecord::Schema.define(version: 2021_11_12_171819) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.string "content"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "likes", force: :cascade do |t|
     t.integer "likeable_id"
