bd4a0cbcdd8ea80838343562fe96526c056868bc
Ebireni David <ebirenidavid@gmail.com>
2021-11-12 22:39:46 +0100

    clean up

--- app/channels/notification_channel.rb
+++
@@ -1,9 +0,0 @@
-class NotificationChannel < ApplicationCable::Channel
-    def subscribed
-      stream_from "NotificationChannel#{current_user.id}"
-    end
-  
-    def unsubscribed
-      # Any cleanup needed when channel is unsubscribed
-    end
-  end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -44,7 +44,7 @@ Rails.application.configure do
   # config.action_cable.mount_path =  '/cable'
   # config.action_cable.url = 'wss://we-share-tweets.herokuapp.com/cable'
   config.action_cable.allowed_request_origins = ['https://we-share-tweets.herokuapp.com/' ]
-  config.action_cable.disable_request_forgery_protection = true
+  # config.action_cable.disable_request_forgery_protection = true
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
