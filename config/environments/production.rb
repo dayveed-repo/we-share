eab100076ef541f8fd552e68a23b7f82c002861d
Ebireni David <ebirenidavid@gmail.com>
2021-11-12 21:40:59 +0100

    actioncable config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -41,8 +41,8 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Mount Action Cable outside main process or domain.
-  # config.action_cable.mount_path = nil
-  # config.action_cable.url = 'wss://we-share-tweets.herokuapp.com/cable'
+  config.action_cable.mount_path =  '/cable'
+  config.action_cable.url = 'wss://we-share-tweets.herokuapp.com/cable'
   config.action_cable.allowed_request_origins = ['https://we-share-tweets.herokuapp.com/', /http:\/\/example.*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
